18162f0936625da5fb7e5e9054f2d278bebdf90d
Martin Beneš <martinbenes1996@gmail.com>
2020-05-18 11:16:03 +0200

    version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setuptools.setup(
   name = 'covid19dh',
-  version = '0.1.3',
+  version = '0.1.4',
   author = 'Covid-19 Data Hub',
   author_email = 'martinbenes1996@gmail.com',
   description = 'Unified data hub for a better understanding of COVID-19 https://covid19datahub.io',
@@ -14,7 +14,7 @@ setuptools.setup(
   packages=setuptools.find_packages(),
   license='GPL',
   url = 'https://www.covid19datahub.io/',
-  download_url = 'https://github.com/covid19datahub/Python/archive/0.1.3.tar.gz',
+  download_url = 'https://github.com/covid19datahub/Python/archive/0.1.4.tar.gz',
   keywords = ['2019-nCov', 'coronavirus', 'covid-19', 'covid-data', 'covid19-data'],
   install_requires=[],
   package_dir={'': '.'},
